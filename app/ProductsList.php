d19f74ecc760bb7cd034b3b95f0765b4d1863b44
JustJoni <just-joni6@mail.ru>
2022-06-30 16:37:05 +1000

    Рабочий парсинг всех необходимых частей страницы

--- app/ProductsList.php
+++ app/ProductsList.php
@@ -16,11 +16,37 @@ class ProductsList
     public function parse()
     {
 		$crawler = new Crawler($this->page);
-		$crawler = $crawler->filter('.purchases');
-		foreach ($crawler as $domElement) 
+		$test = $crawler->filter('div.purchases.block')->each(function ($node) {
+			
+			if(!empty($node))
+			{
+				$purchasesName = $node->filter('h2 > a')->innerText('Default');
+				var_dump($purchasesName);
+			}
+			$blocks = $node->filter('div.purchase-block')->each(function ($nodeChild) use($purchasesName) {
+				$href = '';
+				$img = '';
+				$title = '';
+			
+				$href = $nodeChild->filter('.properties > div.name > a')->attr('href');
+				$img = $nodeChild->filter('img')->attr('src');
+				$title = $nodeChild->filter('.properties > div.name > a')->innerText('Default');
+
+				var_dump($title);
+				
+				return compact('purchasesName','href','img','title');
+			});
+			
+			
+			$blocks[] = ['purchasesName' => $purchasesName];
+
+			return $blocks;
+		});
+		foreach ($test as $domElement) 
 		{
-			var_dump($domElement->nodeName);
+			print_r($domElement);
 		}
+		
 		
         return NULL;
     }
