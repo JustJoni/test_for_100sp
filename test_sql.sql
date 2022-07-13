5f95a3bfabd22a73d14915e937bf826a9f8308a4
JustJoni <just-joni6@mail.ru>
2022-07-13 13:48:12 +1000

    Добавил доп. таблицу с городами

--- test_sql.sql
+++ test_sql.sql
@@ -2,13 +2,21 @@ CREATE TABLE `purchase_type_test` (
   `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(35) NOT NULL
 ) COLLATE utf8mb4_unicode_ci;
+CREATE TABLE `city_test` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(35) NOT NULL,
+  `url` varchar(35) NOT NULL
+) COLLATE utf8mb4_unicode_ci;
 CREATE TABLE `purchase_test` (
   `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `purchase_id` int NOT NULL,
+  `purchase_id` int unsigned NOT NULL,
+  `city_id` int unsigned NOT NULL,
   `name` varchar(200) NOT NULL,
   `img` varchar(60) NOT NULL,
   `href` varchar(60) NOT NULL
 ) COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE `purchase_test`
-ADD FOREIGN KEY (`purchase_id`) REFERENCES `purchase_type_test` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+ADD FOREIGN KEY (`purchase_id`) REFERENCES `purchase_type_test` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `purchase_test`
+ADD FOREIGN KEY (`city_id`) REFERENCES `city_test` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
